b75901b0582d3abf042f2092246f402a2d348a85
Sam Hamm <samhamm@gmail.com>
2017-07-12 11:33:02 -0700

    middle of class 7/12

--- class-03-lists-loops-box_model/demo/app.js
+++ class-03-lists-loops-box_model/demo/app.js
@@ -1,1 +1,68 @@
 'use strict';
+
+// Arrays
+
+var myCats = ['BuddyCat', 'Alistair', 'Trillian'];
+// "var myCats" is a variable declaration
+// "= ['BuddyCat', 'Alistair', 'Trillian']" is an assignment
+// The "=" sign is called an assignment operator
+
+var randomCrap = [true, 4, 'WTF', myCats];
+
+// We use 'for' loops to iterate over arrays
+
+// for(a var holding a starting index; how many times; increment){
+//  stuff to do to each array element
+// }
+
+// for(condition){
+//   execute the code in this code block
+// }
+
+for(var i = 0; i < myCats.length; i++) {
+  alert(myCats[i].toUpperCase());
+}
+
+// i++ is an incrementer; "++" is the "increment operator"
+
+for(var i = 0; i < myCats.length; i++) {
+  if(myCats[i] === 'Trillian'){
+    console.log('Trill!!!!')
+  } else {
+    console.log(':-( not Trill :-(')
+  }
+}
+
+// if(condition is true){
+//   do the stuff in the code block
+// }
+
+// if(condition is true){
+//   do the stuff in the code block
+// } else {
+//   do this other stuff
+// }
+
+// if(condition is true){
+//   do the stuff in the code block
+// } else if (this other condition is true) {
+//   do this other stuff
+// } else {
+//   do this last thing as a catch-all case
+// }
+
+// while(condition is true){
+// keep doing this stuff in the code block until the condition is false
+// }
+
+var x = 0
+while(x < 7){
+  console.log('x =', x);
+  x++;
+}
+
+var y = 0;
+do {
+  console.log(y, 'is the value of y');
+  y++
+} while (y < 0);
