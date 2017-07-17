cdf18a247dd032bcc16dba8697e3f49dd8bd425e
Sam Hamm <samhamm@gmail.com>
2017-07-17 12:08:20 -0700

    end of class 7/17

---
+++ class-06-dom-objects-domain_modeling/demo/app.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var days = ['Monday', 'Tuesday', 'Wednesday', 'Thor\'s Day', 'Friday', 'Saturday', 'Sunday'];
+
+// Favorite Seattle beaches
+
+ var alki = {
+   name: 'Alki',
+   seagullCount: [34, 56, 78, 14, 3000, 9000, 1200],
+   render: function(){
+     var alkiUL = document.getElementById('alki');
+     // This will render the seagull data array to the DOM
+     // We'll need a 'for' loop to iterate over the array
+     for(var i = 0; i < this.seagullCount.length; i++){
+       // There are three parts to this process:
+       // 1. Create an element
+       var liEl = document.createElement('li');
+       // 2. Give it content
+       liEl.textContent = days[i] + ': ' + this.seagullCount[i];
+       // 3. Append it to a certain place in the DOM
+       // parentElement.appendChild(childElement)
+       alkiUL.appendChild(liEl);
+     }
+   }
+ };
+
+ alki.render();
+
+var goldenGarden = {
+  name: 'Golden Garden',
+  seagullCount: [3,5,4,7,5,9,8],
+  render: function(){
+    var golden = document.getElementById('gg');
+    // This will render the seagull data array to the DOM
+    // We'll need a 'for' loop to iterate over the array
+    for(var i = 0; i < this.seagullCount.length; i++){
+      // There are three parts to this process:
+      // 1. Create an element
+      var liEl = document.createElement('li');
+      // 2. Give it content
+      liEl.textContent = days[i] + ': ' + this.seagullCount[i];
+      // 3. Append it to a certain place in the DOM
+      // parentElement.appendChild(childElement)
+      golden.appendChild(liEl);
+    }
+  }
+};
+
+goldenGarden.render();
+
+var edmondsBeach = {
+  name: 'edmondsBeach',
+  seagullCount: [52,46,76,87,365,13,38],
+  render: function(){
+    var eddie = document.getElementById('edmonds');
+    // This will render the seagull data array to the DOM
+    // We'll need a 'for' loop to iterate over the array
+    for(var i = 0; i < this.seagullCount.length; i++){
+      // There are three parts to this process:
+      // 1. Create an element
+      var liEl = document.createElement('li');
+      // 2. Give it content
+      liEl.textContent = days[i] + ': ' + this.seagullCount[i];
+      // 3. Append it to a certain place in the DOM
+      // parentElement.appendChild(childElement)
+      eddie.appendChild(liEl);
+    }
+  }
+};
+
+edmondsBeach.render();
