4fcffb9a52366eedfddf2b9ff57ac0f722056615
Sam Hamm <samhamm@gmail.com>
2017-07-18 11:38:36 -0700

    middle of class 7/18

--- class-07-oop-constructors-tables/buildout-of-yesterday/js/app.js
+++ class-07-oop-constructors-tables/buildout-of-yesterday/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am'];
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm'];
 
 var pikePlace = {
   minCustomers: 23,
@@ -14,10 +14,24 @@ var pikePlace = {
   },
   cookiesSoldEachHour: [],
   calcCookiesSoldEachHour: function(){
-
+    this.calcCustomersPerHour();
+    for(var i = 0; i < hours.length; i++){
+      this.cookiesSoldEachHour.push(Math.ceil(this.customersPerHour[i] * this.avgCookiesPerSale));
+      this.totalCookiesPerDay += this.cookiesSoldEachHour[i];
+      console.log(this.totalCookiesPerDay, 'running daily cookie total');
+    }
   },
   totalCookiesPerDay: 0,
   render: function(){
-
+    var pikeList = document.getElementById('pike');
+    this.calcCookiesSoldEachHour();
+    for(var i = 0; i < hours.length; i++){
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + ': ' + this.cookiesSoldEachHour[i] + ' cookies';
+      pikeList.appendChild(liEl);
+    }
+    liEl = document.createElement('li');
+    liEl.textContent = 'Total: ' + this.totalCookiesPerDay + ' cookies';
+    pikeList.appendChild(liEl);
   }
 };
--- class-07-oop-constructors-tables/demo-constructor-functions/js/app.js
+++ class-07-oop-constructors-tables/demo-constructor-functions/js/app.js
@@ -4,24 +4,54 @@
 
 // Let's make an object literal of one of you:
 
-var gregoryLiteral = {
-  firstName: 'Gregory',
-  lastInitial: 'D',
-  hometown: 'Aurora, CO',
-  faveNumber: 7,
-  faveColor: 'blue',
-  isCodeNinja: true,
-  introduction: function() {
+// var gregoryLiteral = {
+//   firstName: 'Gregory',
+//   lastInitial: 'D',
+//   course: '201d24',
+//   hometown: 'Aurora, CO',
+//   faveNumber: 7,
+//   faveColor: 'blue',
+//   isCodeNinja: true,
+//   introduction: function() {
+//     console.log('Hi, my name is ' + this.firstName + ' and I am from ' + this.hometown);
+//   }
+// };
+
+// I would need 276 LOC (lines) of code to model our entire class this way
+// (23 students * 12 lines each)
+
+// What if... I had a function to make these many similar objects for me? A function where I could pass in a student's unique characteristics as arguments, and then that function would make the individual objects? We have these things, and they are called CONSTRUCTOR FUNCTIONS.
+
+function Student(firstName, lastInitial, hometown, faveNumber, faveColor){
+  this.firstName = firstName;
+  this.lastInitial = lastInitial;
+  this.course = '201d24';
+  this.hometown = hometown;
+  this.faveNumber = faveNumber;
+  this.faveColor = faveColor;
+  this.isCodeNinja = true;
+  this.introduction = function() {
     console.log('Hi, my name is ' + this.firstName + ' and I am from ' + this.hometown);
-  }
-};
+  };
+  mahClass.push(this);
+}
 
-// I would need 253 LOC (lines) of code to model our entire class this way
-// (23 students * 11 lines each)
-
-// What if... I had a function to make these many similar objects for me? A function where I could pass in a student's unique characteristics as arguments, and then that function would make the individual objects?
-
-// var mahClass = [];
+var mahClass = [];
 // What could that array for? Why would I need one?
+new Student('Gregory', 'D', 'Aurora, CO', 7, 'blue');
+new Student('Ivette', 'C', 'Guatemala', 9, 'blue');
 
-// Now all I need is XXX lines of code to represent all 23 students instead of XXX. What a bargain!
+// 15 lines of code to make two student objects; which means I would need 21 lines of code to make objects of the rest of the class. Thus, 38 lines of code to model the entire class. That's A LOT less than 276 LOC that I'd need with object literals.
+
+for(var i = 0; i < mahClass.length; i++){
+  console.log(mahClass[i].firstName);
+}
+
+function studentsInTheDOM() {
+  var studentList = document.getElementById('students');
+  for(var i = 0; i < mahClass.length; i++){
+    var liEl = document.createElement('li');
+    liEl.textContent = mahClass[i].firstName;
+    studentList.appendChild(liEl);
+  }
+}
