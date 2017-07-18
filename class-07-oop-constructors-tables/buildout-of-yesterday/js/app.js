c2a28d2dc37e7e99f54365eb6253854e4bf5281d
Sam Hamm <samhamm@gmail.com>
2017-07-18 09:23:08 -0700

    first basic object set up

--- class-07-oop-constructors-tables/buildout-of-yesterday/js/app.js
+++ class-07-oop-constructors-tables/buildout-of-yesterday/js/app.js
@@ -2,8 +2,20 @@
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am'];
 
-var pikeplace = {
+var pikePlace = {
   minCustomers: 23,
   maxCustomers: 65,
   avgCookiesPerSale: 6.3,
+  customersPerHour: [],
+  calcCustomersPerHour: function(){
+
+  },
+  cookiesSoldEachHour: [],
+  calcCookiesSoldEachHour: function(){
+
+  },
+  totalCookiesPerDay: 0,
+  render: function(){
+    
+  }
 };
